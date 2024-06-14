8b5b968163ad31a056cdcd90c47f9b9f2edb8ae8
isaoasis <164797626+isaoasis@users.noreply.github.com>
2024-06-14 10:32:52 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
 
     # Configuración de base de datos 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgres://default:NjXqClpM73Kx@ep-curly-dawn-a4hwnczb-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgres://default:NjXqClpM73Kx@ep-curly-dawn-a4hwnczb-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
 
 class ProductionConfig(Config):
     DEBUG = False
